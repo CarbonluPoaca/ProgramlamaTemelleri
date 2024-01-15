b9d941526436716fbecd6e56f4a626bbe7abef26
Carbon <tahabesirkose@gmail.com>
2024-01-15 23:20:12 +0300

    15.01.2024 ders sonu

--- Math12012024/Program.cs
+++ Math12012024/Program.cs
@@ -1,21 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Math12012024;
-
-
-
-
 DortIslem dortIslem = new DortIslem();
-bool b = true;
-while (b == true)
-{
-    string a = Console.ReadLine();
-    dortIslem.kufur(a);
-}
-
-
-
-
 Console.WriteLine("--------------------");
 dortIslem.toplama(2,3);
 Console.WriteLine("----------");
